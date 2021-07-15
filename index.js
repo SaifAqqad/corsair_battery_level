0ab5f5ffabc30e326663ff19e6f2a9c1b9504a7e
Saif Aqqad <saifo1999@gmail.com>
2021-07-15 17:57:13 +0300

    Switch tray lib to node-systray

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import { NotifyIcon, Icon, Menu } from "not-the-systray";
-import HID from "node-hid"
-const DATA_REQ = [0xC9, 0x64],
+require('node-hide-console-window').hideConsole();
+const HID = require('node-hid'),
+    SysTray = require('systray2').default,
+    path = require('path'),
+    DATA_REQ = [0xC9, 0x64],
     CORSAIR_VID = 0x1B1C,
     KNOWN_PIDS = {
         0x0A38: "HS70 Wireless",
@@ -26,66 +28,61 @@ const DATA_REQ = [0xC9, 0x64],
         4: "Fully charged",
         5: "Charging"
     },
-    CON_COLOR = {
-        default: "\x1b[0m",
-        red: "\x1b[31m",
-        green: "\x1b[32m"
-    },
     TRAY_ICONS = {
-        default: Icon.load("icons/headphones.ico", Icon.small),
-        charging: Icon.load("icons/battery-charging.ico", Icon.small),
-        10: Icon.load("icons/battery-wireless.ico", Icon.small),
-        9: Icon.load("icons/battery-wireless-90.ico", Icon.small),
-        8: Icon.load("icons/battery-wireless-80.ico", Icon.small),
-        7: Icon.load("icons/battery-wireless-70.ico", Icon.small),
-        6: Icon.load("icons/battery-wireless-60.ico", Icon.small),
-        5: Icon.load("icons/battery-wireless-50.ico", Icon.small),
-        4: Icon.load("icons/battery-wireless-40.ico", Icon.small),
-        3: Icon.load("icons/battery-wireless-30.ico", Icon.small),
-        2: Icon.load("icons/battery-wireless-20.ico", Icon.small),
-        1: Icon.load("icons/battery-wireless-10.ico", Icon.small),
-        0: Icon.load("icons/battery-wireless-0.ico", Icon.small),
-    },
-    TRAY_OPTIONS = {
-        icon: TRAY_ICONS["default"],
-        tooltip: "Corsair battery level",
-        onSelect: (event) => show_menu(event)
+        default: path.join(__dirname, "icons/headphones.ico"),
+        charging: path.join(__dirname, "icons/battery-charging.ico"),
+        10: path.join(__dirname, "icons/battery-wireless.ico"),
+        9: path.join(__dirname, "icons/battery-wireless-90.ico"),
+        8: path.join(__dirname, "icons/battery-wireless-80.ico"),
+        7: path.join(__dirname, "icons/battery-wireless-70.ico"),
+        6: path.join(__dirname, "icons/battery-wireless-60.ico"),
+        5: path.join(__dirname, "icons/battery-wireless-50.ico"),
+        4: path.join(__dirname, "icons/battery-wireless-40.ico"),
+        3: path.join(__dirname, "icons/battery-wireless-30.ico"),
+        2: path.join(__dirname, "icons/battery-wireless-20.ico"),
+        1: path.join(__dirname, "icons/battery-wireless-10.ico"),
+        0: path.join(__dirname, "icons/battery-wireless-0.ico"),
     },
     MENU_ITEMS = [
-        { id: 1, text: "Exit" },
+        {
+            title: "Exit",
+            tooltip: "Exit",
+            checked: false,
+            enabled: true,
+            click: () => {
+                tray.kill(false)
+                process.exit(0)
+            }
+        }
     ],
-    VOID_BATTERY_MICUP = 128;
-let tray = new NotifyIcon(TRAY_OPTIONS),
-    menu = new Menu(MENU_ITEMS),
-    device_info = null,
-    device_hid = null;
-init();
-
-function init() {
+    TRAY_OPTIONS = {
+        menu: {
+            icon: TRAY_ICONS["default"],
+            title: "Corsair battery level",
+            tooltip: "Corsair battery level",
+            items: MENU_ITEMS
+        },
+        debug: false,
+        copyDir: true
+    },
+    VOID_BATTERY_MICUP = 128,
+    tray = new SysTray(TRAY_OPTIONS);
+tray.onClick(event => {
+    if (event.item.click != null) {
+        event.item.click()
+    }
+})
+let device_info = device_hid = null;
+tray.ready().then(() => {
     [device_hid, device_info] = get_device();
     if (!device_hid) {
-        console.error("could not find a device");
         process.exit(1);
     }
     device_info.full_name = `${device_info.manufacturer} ${KNOWN_PIDS[device_info.productId]}`
     device_hid.setNonBlocking(1);
-    device_hid.on('data', handle_data);
-    process.stdout.write(`Found Device: ${device_info.full_name}\n`);
-    process.stdout.write(`\tBattery level\t\tCurrent state\n`);
+    device_hid.on('data', update_tray);
     device_hid.resume();
-}
-
-function show_menu(event) {
-    if (!event.rightButton){
-        device_hid.write(DATA_REQ);
-        return
-    }
-    const ret = menu.showSync(event.mouseX, event.mouseY);
-    if (ret === 1) {
-        tray.remove()
-        process.exit(0)
-    }
-}
+})
 
 function get_device() {
     let dList = HID.devices(), hidDevice, infoObj;
@@ -106,27 +103,10 @@ function get_device() {
     return [hidDevice, infoObj];
 }
 
-// [  0    ,  1  ,   2   ,  3  ,  4  ]
-// reportId,     ,battery,     ,state
-function handle_data([, , battery, , state]) {
+function update_tray([, , battery, , state]) {
     if (battery > VOID_BATTERY_MICUP) {
         battery = battery - VOID_BATTERY_MICUP;
     }
-    update_console(battery, state)
-    update_tray(battery, state)
-}
-
-function update_console(battery, state) {
-    process.stdout.clearLine(0);
-    process.stdout.cursorTo(0);
-    let bColor = battery >= 40 ? CON_COLOR.green : CON_COLOR.red,
-        sColor = state === 0 || state === 2 ? CON_COLOR.red : CON_COLOR.green;
-    process.stdout.write(`\
-    \t${bColor}${battery}\t\t\t${sColor}${DEVICE_STATES[state]}${CON_COLOR.default}`);
-
-}
-
-function update_tray(battery, state) {
     let icon, tooltip;
     if (state === 0 || DEVICE_STATES[state] === undefined) { //disconnected
         icon = TRAY_ICONS["default"];
@@ -136,7 +116,15 @@ function update_tray(battery, state) {
         tooltip = `${device_info.full_name}: ${DEVICE_STATES[state]}`;
     } else {
         icon = TRAY_ICONS[Math.floor(battery / 10)];
-        tooltip = `${device_info.full_name}: ${DEVICE_STATES[state]}\nBattery: ${battery}%`;
+        tooltip = `${device_info.full_name}: ${DEVICE_STATES[state]} (${battery}%)`;
     }
-    tray.update({ icon, tooltip });
+    tray.sendAction({
+        type: 'update-menu',
+        menu: {
+            icon,
+            tooltip,
+            title: tooltip,
+            items: MENU_ITEMS
+        }
+    });
 }
